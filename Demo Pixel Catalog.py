951a500844c165c6db5301baa5aaa6feda9a9db9
dmoore247 <dmoore247>
2021-03-23 16:46:21 +0000

    Polish

--- Demo Pixel Catalog.py
+++ Demo Pixel Catalog.py
@@ -1,16 +1,24 @@
 # Databricks notebook source
-from databricks.pixel import ObjectFrames, Catalog
+# MAGIC %md # Demo Pixels Catalog
 
 # COMMAND ----------
 
-help(Catalog)
+# MAGIC %pip install -vv -e git+https://github.com/dmoore247/pixels#egg=databricks.pixel
 
 # COMMAND ----------
 
-path = 'dbfs:/databricks-datasets/med-images/camelyon16/'
-df = Catalog.catalog(spark, path)
-
+# DBTITLE 1,1. List Files
+from databricks.pixel import Catalog
+display(
+  Catalog.catalog(spark, 'dbfs:/databricks-datasets/med-images/camelyon16/', pattern='normal_???.tif')
+)
 
 # COMMAND ----------
 
+from databricks.pixel import Catalog
+df = Catalog.catalog(spark, 'dbfs:/databricks-datasets/med-images/camelyon16/', pattern='normal_???.tif')
 display(df)
+
+# COMMAND ----------
+
+
