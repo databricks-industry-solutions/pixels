577630711b465c19c75bb6bfd558cd58b8dd8c28
Douglas Moore <dmoore247@users.noreply.github.com>
2021-03-28 19:23:26 -0400

    Remove positioning tags on plot

--- databricks/pixels/dicoms.py
+++ databricks/pixels/dicoms.py
@@ -54,11 +54,8 @@ class DicomFrames(ObjectFrames):
         for i in images:
             path = i[0].replace('dbfs:','/dbfs')
             ds = dcmread(path)
-            print(ds)
-            tags = i[1]
-            loc = self._get_loc(tags)
 
-            plt.subplot(2,2,loc)
+            plt.subplot(1,1,0)
             plt.imshow(ds.pixel_array, cmap="gray")
         
         plt.show()
