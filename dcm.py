53c6f384df9f2f9008c1961c951e22026825f24f
dmoore247 <dmoore247>
2021-04-12 01:40:02 +0000

    Cleaned up demo notebooks

--- dcm.py
+++ dcm.py
@@ -1,50 +1,96 @@
 # Databricks notebook source
-# MAGIC %conda install -c conda-forge gdcm -y
+# MAGIC %md # Scale Dicom based image processing
+# MAGIC - Use `databricks.pixels` python package for simplicity
+# MAGIC - Scale up Image processing over multiple-cores and nodes
+# MAGIC - Delta lake & Delta Engine accelerate metadata research.
+# MAGIC - Delta lake (optionally) to speed up small file processing
+# MAGIC - Mix of Spark and Python processing
+# MAGIC - Uses pydicom python package as core library
+# MAGIC 
+# MAGIC tags: douglas moore, dicom, dcm, pre-processing, visualization, repos, python, package, image catalog, mamograms
 
 # COMMAND ----------
 
-# MAGIC %pip install -r requirements.txt
+# MAGIC %md ## Setup
 
 # COMMAND ----------
 
-from databricks.pixels import Catalog
+# %conda install -c conda-forge gdcm -y
+
+# COMMAND ----------
+
+#%pip install -r requirements.txt
+
+# COMMAND ----------
+
+# MAGIC %md ## Load Dicom Images
+
+# COMMAND ----------
+
+from databricks.pixels import Catalog, DicomFrames
+
+# COMMAND ----------
+
 df = Catalog.catalog(spark, "dbfs:/FileStore/shared_uploads/douglas.moore@databricks.com/benigns/")
 display(df)
 
 # COMMAND ----------
 
-from databricks.pixels import DicomFrames
+# MAGIC %md ## Extract Metadata from the Dicom images
+
+# COMMAND ----------
+
 dcm_df = DicomFrames(df).withMeta()
 display(dcm_df)
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
-plots = DicomFrames(dcm_df.repartition(64)).plot()
+# MAGIC %md ## Save Image (metadata) Catalog
 
 # COMMAND ----------
 
-# MAGIC %fs ls dbfs:/FileStore/plots/pixels/
+dcm_df.write.format("delta").mode("overwrite").save("/tmp/douglas_moore/dcm_catalog")
 
 # COMMAND ----------
 
-plots._files
+# DBTITLE 1,Read and cache metadata from storage
+dcm_df = spark.read.format("delta").load("/tmp/douglas_moore/dcm_catalog")
+dcm_df.createOrReplaceTempView("dicom_images")
 
 # COMMAND ----------
 
-plots._repr_html_()
+# MAGIC %md ## Analyze Metadata
 
 # COMMAND ----------
 
-from databricks.pixels import PlotResult
+# MAGIC %sql select meta:img_min, meta:img_max from dicom_images where array_contains( path_tags, 'patient7747' )
 
 # COMMAND ----------
 
-help(PlotResult)
+# DBTITLE 1,Analyze Dicom metadata
+# MAGIC %sql
+# MAGIC SELECT meta:['00100010'].Value[0].Alphabetic as patient_name, meta:img_min, meta:img_max, path 
+# MAGIC FROM dicom_images 
+# MAGIC order by patient_name
 
 # COMMAND ----------
 
+# MAGIC %md ## Filter Dicom Images
 
+# COMMAND ----------
+
+dcm_df_filtered = dcm_df.filter('meta:img_max < 1000')
+dcm_df_filtered.count()
+
+# COMMAND ----------
+
+# MAGIC %md ## Display Dicom Images
+
+# COMMAND ----------
+
+plots = DicomFrames(dcm_df_filtered).plotx()
+plots
+
+# COMMAND ----------
+
+# MAGIC %md ## Appendix
