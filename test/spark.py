930f18bfb6745243d01fefd49eac81cc4357ba80
Douglas Moore <dmoore247@users.noreply.github.com>
2021-05-22 15:19:13 -0400

    Make desktop experience easier by setting env variable

--- test/spark.py
+++ test/spark.py
@@ -2,6 +2,14 @@ def get_spark():
     import warnings
     from pyspark.sql import SparkSession
     warnings.simplefilter('ignore', DeprecationWarning)
+    import os
+
+    # 
+    os.environ['DEBUG_IGNORE_VERSION_MISMATCH'] = "1"
+
     return (SparkSession.builder.appName("pixels").getOrCreate())
 
 spark = get_spark()
+
+if __name__ == "__main__":
+    print(f"Spark Version: {spark.version}")
