159b060413383c270c2bca78c7c92e0fad33b0dc
Douglas Moore <dmoore247@users.noreply.github.com>
2021-08-10 17:13:16 -0400

    add image shape

--- databricks/pixels/dicom_meta_extractor.py
+++ databricks/pixels/dicom_meta_extractor.py
@@ -1,8 +1,39 @@
 from pyspark.ml.pipeline import Transformer
 import pyspark.sql.types as t
 
-from pyspark.sql.functions import col
-from databricks.pixels.dicom_udfs import dicom_meta_udf
+from pyspark.sql.functions import col, udf
+
+@udf
+def dicom_meta_udf(local_path:str, deep:bool = True) -> dict:
+    """Extract metadata from header of dicom image file"""
+    from pydicom import dcmread
+    from pydicom.errors import InvalidDicomError
+    import numpy as np
+    try:
+        with dcmread(local_path) as ds:
+            js = ds.to_json_dict()
+            # remove binary images
+            if '60003000' in js:
+                del js['60003000']
+            if '7FE00010' in js:
+                del js['7FE00010']
+
+            if deep:
+                a = ds.pixel_array
+                a.flags.writeable = False
+                js['hash'] = hash(a.data.tobytes())
+                js['img_min'] = np.min(a)
+                js['img_max'] = np.max(a)
+                js['img_avg'] = np.average(a)
+                js['img_shape'] = a.shape
+            
+            return str(js)
+    except InvalidDicomError as err:
+        return str({
+            'error': str(err),
+            'local_path': local_path
+        })
+
 
 class DicomMetaExtractor(Transformer):
     # Day extractor inherit of property of Transformer 
@@ -10,24 +41,18 @@ class DicomMetaExtractor(Transformer):
         self.inputCol = inputCol #the name of your columns
         self.outputCol = outputCol #the name of your output column
         self.basePath = basePath
-    def this():
-        #define an unique ID
-        this(Identifiable.randomUID("DicomMetaExtractor"))
-    def copy(extra):
-        defaultCopy(extra)
+    
     def check_input_type(self, schema):
         field = schema[self.inputCol]
         #assert that field is a datetype 
         if (field.dataType != t.StringType()):
             raise Exception('DicomMetaExtractor input type %s did not match input type StringType' % field.dataType)
+        
+        #TODO check string prefix for local filetype (or extended a resolvable scheme)
+
     def _transform(self, df):
         self.check_input_type(df.schema)
-        return self._transform_impl(df, self.inputCol, self.outputCol)
-
-    def _transform_impl(self, df, inputCol, outputCol):
-        print("calling dicom_meta_")
-        return (df.withColumn(outputCol, dicom_meta_udf(col(inputCol))))
-
+        return (df.withColumn(self.outputCol, dicom_meta_udf(col(self.inputCol))))
 
     
 if __name__ == '__main__':
