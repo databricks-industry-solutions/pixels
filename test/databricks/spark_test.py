1ecadfe3eeabc68834458bfa7ced407ead542f23
Douglas Moore <douglas.moore@databricks.com>
2023-10-11 21:10:42 -0400

    simplify spark test

--- test/databricks/spark_test.py
+++ test/databricks/spark_test.py
@@ -8,21 +8,6 @@ def spark() -> SparkSession:
   # have access to this SparkSession by default.
   return SparkSession.builder.getOrCreate()
 
-# Now add your unit tests.
-
-# For example, here is a unit test that must be run on the
-# cluster in the remote Databricks workspace.
-# This example determines whether the specified cell in the
-# specified table contains the specified value. For example,
-# the third colum in the first row should contain the word "Ideal":
-#
-# +----+-------+-------+-------+---------+-------+-------+-------+------+-------+------+
-# |_c0 | carat | cut   | color | clarity | depth | table | price | x    | y     | z    |
-# +----+-------+-------+-------+---------+-------+-------+-------+------+-------+------+
-# | 1  | 0.23  | Ideal | E     | SI2     | 61.5  | 55    | 326   | 3.95 | 3. 98 | 2.43 |
-# +----+-------+-------+-------+---------+-------+-------+-------+------+-------+------+
-# ...
-#
 
 def test_spark_type(spark):
   assert spark is not None
@@ -30,5 +15,5 @@ def test_spark_type(spark):
     
 def test_spark(spark):
   spark.sql('USE default')
-  data = spark.sql('SELECT * FROM diamonds')
-  assert data.collect()[0][2] == 'Ideal'
+  data = spark.sql('SELECT id FROM range(100) order by id asc')
+  assert data.collect()[2][0] == 2
