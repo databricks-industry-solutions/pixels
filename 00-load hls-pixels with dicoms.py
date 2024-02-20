8cab2b2db2893bfdc4a56c5d8a05875d97856a4d
dmoore247 <dmoore247>
2024-02-20 19:04:35 +0000

    Add more UC niceities.
    Create a view on top of the object_catalog table
    Read images from Volume by default.

---
+++ 00-load hls-pixels with dicoms.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %fs ls /Volumes/douglas_moore/pixels_solacc/raw_dicoms
+
+# COMMAND ----------
+
+# MAGIC %fs cp -r s3://hls-eng-data-public/dicom/ddsm/ /Volumes/douglas_moore/pixels_solacc/raw_dicoms
+
+# COMMAND ----------
+
+
