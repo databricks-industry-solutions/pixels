3a8d8ee4a06207f9ce8d97c58d98e44fa756bccd
Emanuele Rinaldi <emanuele.rinaldi@databricks.com>
2024-02-28 14:49:09 -0500

    styled

--- tests/dbx/test_stream.py
+++ tests/dbx/test_stream.py
@@ -1,18 +1,15 @@
 import pytest
 from databricks.connect import DatabricksSession
 from databricks.sdk.core import DatabricksError
-from pyspark.sql import SparkSession
-from dbx.pixels import Catalog
-
-from dbx.pixels.dicom import DicomMetaExtractor, DicomThumbnailExtractor
-
 from databricks.sdk.runtime import dbutils
+from pyspark.sql import SparkSession
 
-from dbx.pixels.version import __version__
+from dbx.pixels import Catalog
 
 FILE_PATH = "s3://hls-eng-data-public/dicom/ddsm/benigns/patient0007/"
 TABLE = "main.pixels_solacc.object_catalog_stream"
 CHECKPOINT_BASE_PATH = "/tmp/pixels_acc_stream_test/checkpoints/"
+
 
 @pytest.fixture()
 def spark() -> SparkSession:
@@ -23,11 +20,12 @@ def spark() -> SparkSession:
     """
     return DatabricksSession.builder.getOrCreate()
 
+
 @pytest.fixture(autouse=True)
 def setup(spark: SparkSession):
     spark.sql("CREATE DATABASE IF NOT EXISTS main.pixels_solacc")
     yield
-    
+
     try:
         checkpoints = list(dbutils.fs.ls(CHECKPOINT_BASE_PATH))
         if len(checkpoints) > 0:
@@ -43,10 +41,12 @@ def setup(spark: SparkSession):
 
 def test_catalog_stream(spark: SparkSession):
     catalog = Catalog(spark, table=TABLE)
-    catalog_df = catalog.catalog(path=FILE_PATH, streaming=True, streamCheckpointBasePath=CHECKPOINT_BASE_PATH)
+    catalog_df = catalog.catalog(
+        path=FILE_PATH, streaming=True, streamCheckpointBasePath=CHECKPOINT_BASE_PATH
+    )
 
     assert catalog_df is not None
-    
+
     catalog.save(df=catalog_df)
 
     assert catalog.load().count() == 4
