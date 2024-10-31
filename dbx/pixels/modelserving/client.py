39e11530bdb9f5931fe018394232cbbd9644340e
erinaldidb <erinaldidb>
2024-10-31 21:42:32 +0000

    Added MONAILabel serving endpoint client transformer

---
+++ dbx/pixels/modelserving/client.py
@@ -0,0 +1,36 @@
+import time
+import pandas as pd
+from pyspark.sql.functions import pandas_udf, col
+from typing import Iterator
+from pyspark.ml.pipeline import Transformer
+from dbx.pixels.modelserving.serving_endpoint_client import MONAILabelClient
+
+
+class MONAILabelTransformer(Transformer):
+    """
+    Transformer class to generate autosegmentations of DICOM files using MONAILabel serving endpoint.
+    """
+
+    def __init__(self, endpoint_name="pixels-monai", inputCol="meta"):
+        self.inputCol = inputCol
+        self.endpoint_name = endpoint_name
+
+    def _transform(self, df):
+        @pandas_udf("result string, error string")
+        def autosegm_monai_udf(iterator: Iterator[pd.Series]) -> Iterator[pd.DataFrame]:
+            client = MONAILabelClient(self.endpoint_name)
+
+            for s in iterator:
+                results, errors = [], []
+                for image_id in s:
+                    result, error = client.predict(image_id)
+                    results.append(result)
+                    errors.append(error)
+
+                yield pd.DataFrame({"result": results, "error": errors})
+
+        return df \
+                .selectExpr(f"{self.inputCol}:['0020000E'].Value[0] as image_id") \
+                .distinct() \
+                .withColumn("segmentation_result", autosegm_monai_udf(col("image_id"))) \
+                .selectExpr("image_id", "segmentation_result.*")
---
+++ dbx/pixels/modelserving/serving_endpoint_client.py
@@ -0,0 +1,27 @@
+import os
+from mlflow.deployments import get_deploy_client
+
+class MONAILabelClient:
+    def __init__(self, endpoint_name, max_retries=3, request_timeout_sec=300):
+        os.environ["MLFLOW_HTTP_REQUEST_MAX_RETRIES"] = str(max_retries)
+        os.environ["MLFLOW_HTTP_REQUEST_TIMEOUT"] = str(request_timeout_sec)
+        os.environ["MLFLOW_DEPLOYMENT_PREDICT_TIMEOUT"] = str(request_timeout_sec)
+
+        self.client = get_deploy_client("databricks")
+        self.endpoint = endpoint_name 
+        self.max_retries = max_retries
+    
+    def predict(self, image_id, iteration=0, prev_error=None):
+      if iteration > self.max_retries:
+        return ("", str(prev_error))
+      
+      try:
+        response =  self.client.predict(
+                endpoint=self.endpoint,
+                inputs={"dataframe_split": {"columns": ["image_id"], "data": [[image_id]]}}
+            )
+        return (response.predictions[0]['0'], "")
+      except Exception as e:
+        if "torch.OutOfMemoryError: CUDA out of memory" in str(e):
+          return ("", str(e))
+        return self.predict(image_id, iteration + 1, prev_error=str(e))
