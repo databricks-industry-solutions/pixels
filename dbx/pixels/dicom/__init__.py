445387fae906f097f76e64c031f7b0c6dbd6df8d
erinaldidb <erinaldidb>
2024-12-03 22:57:12 +0000

    added Anonymizer in dbx package

--- dbx/pixels/dicom/__init__.py
+++ dbx/pixels/dicom/__init__.py
@@ -6,3 +6,4 @@ from dbx.pixels.dicom.dicom_plot import DicomPlot
 from dbx.pixels.dicom.dicom_thumbnail_extractor import DicomThumbnailExtractor
 from dbx.pixels.dicom.dicom_udfs import dicom_meta_udf
 from dbx.pixels.dicom.dicom_xform_patcher import DicomPatcher
+from dbx.pixels.dicom.dicom_meta_anonymizer import DicomMetaAnonymizer
