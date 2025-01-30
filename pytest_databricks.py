7b6153ed7bed80c017edec9a0d2ef701ddde75e1
Emanuele Rinaldi <emanuele.rinaldi@databricks.com>
2025-01-30 14:50:19 -0500

    fix github action

--- pytest_databricks.py
+++ pytest_databricks.py
@@ -20,11 +20,10 @@ import pytest
 import sys
 import os
 
-print(f"dbx.pixels version: {dbx.pixels.__version__}")
-
 os.environ['DATABRICKS_HOST'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get()
 os.environ['DATABRICKS_TOKEN'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
 
 sys.dont_write_bytecode = True
 
 pytest.main(['--import-mode=importlib', 'tests/dbx/'])
+# COMMAND ----------
