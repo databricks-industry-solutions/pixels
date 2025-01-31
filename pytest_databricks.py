e4218081a1f1555d3416bba25c223d29803fd31a
Emanuele Rinaldi <emanuele.rinaldi@databricks.com>
2025-01-31 15:35:51 -0500

    fixed check

--- pytest_databricks.py
+++ pytest_databricks.py
@@ -13,10 +13,12 @@ logging.getLogger('py4j.java_gateway').setLevel(logging.ERROR)
 
 # COMMAND ----------
 
-import dbx.pixels
+import dbx
 import pytest
 import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath("../"+dbx.__file__))))
 
 os.environ['DATABRICKS_HOST'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get()
 os.environ['DATABRICKS_TOKEN'] = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
