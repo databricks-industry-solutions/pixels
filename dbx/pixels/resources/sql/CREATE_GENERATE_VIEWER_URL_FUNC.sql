1dbe55cd733d1247af2432344ce7e3ce4c7754c2
dmoore247 <dmoore247>
2025-06-12 14:47:33 +0000

    Add Cohort Builder DDL
    Add Cohort Builder as re-usable TVF, update dashboard and installer.

--- dbx/pixels/resources/sql/CREATE_GENERATE_VIEWER_URL_FUNC.sql
+++ dbx/pixels/resources/sql/CREATE_GENERATE_VIEWER_URL_FUNC.sql
@@ -1,6 +1,28 @@
-CREATE OR REPLACE FUNCTION generate_viewer_url(modality STRING, viewer_host STRING, StudyInstanceUID STRING) RETURNS STRING
-RETURN
-CASE 
-    WHEN modality = 'CT' THEN concat('<a target="_blank" rel="noopener noreferrer" href="',viewer_host,'/ohif/monai-label?StudyInstanceUIDs=',StudyInstanceUID,'">view</a>')
-    ELSE concat('<a target="_blank" rel="noopener noreferrer" href="',viewer_host,'/ohif/viewer?StudyInstanceUIDs=',StudyInstanceUID,'">view</a>') 
-END
+CREATE OR REPLACE FUNCTION {UC_SCHEMA}.generate_viewer_url(
+  viewer_host STRING COMMENT "Databricks App URL for the viewer",
+  modality STRING COMMENT "Modality type (e.g., CT, MRI)",
+  ImageType STRING COMMENT "Image type (e.g. AXIAL)",
+  StudyInstanceUID STRING COMMENT "Study Instance UID for the DICOM images, this is a viewer parameter"
+  )
+  RETURNS STRING
+  COMMENT "Function to generate a viewer URL based on the image modality"
+  RETURN CASE
+    WHEN
+      modality = 'CT' and ImageType LIKE '%AXIAL%'
+    THEN
+      concat(
+        '<a target="_blank" rel="noopener noreferrer" href="',
+        viewer_host,
+        '/ohif/monai-label?StudyInstanceUIDs=',
+        StudyInstanceUID,
+        '">view</a>'
+      )
+    ELSE
+      concat(
+        '<a target="_blank" rel="noopener noreferrer" href="',
+        viewer_host,
+        '/ohif/viewer?StudyInstanceUIDs=',
+        StudyInstanceUID,
+        '">view</a>'
+      )
+  END
