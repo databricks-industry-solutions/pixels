cc4ec0037d1ec7785e41bed477ad81ae7e0e34e5
Emanuele Rinaldi <emanuele.rinaldi@databricks.com>
2025-10-24 14:37:25 -0400

    partial frame with basic offset table logic

--- dbx/pixels/resources/lakehouse_app/utils/partial_frames.py
+++ dbx/pixels/resources/lakehouse_app/utils/partial_frames.py
@@ -7,6 +7,10 @@ import requests
 import dbx.pixels.version as dbx_pixels_version
 from dbx.pixels.databricks_file import DatabricksFile
 
+from dbx.pixels.logging import LoggerProvider
+
+logger = LoggerProvider("PartialFramesUtils")
+
 """
 Utility functions for handling partial frame extraction from DICOM files in Databricks Volumes.
 
@@ -70,6 +74,78 @@ def get_file_metadata(token: str, db_file: DatabricksFile):
         to_return.update(ds.file_meta.to_json_dict())
         return to_return
 
+def extract_from_basic_offsets(f, pixel_data_pos, endianness):
+    frames = []
+
+    frame_index = 0
+    f.seek(pixel_data_pos)
+    buffer = f.read(100)
+    basic_offset_pos = buffer.find(b"\xFE\xFF\x00\xE0")
+    f.seek(pixel_data_pos + basic_offset_pos)
+    basic_offsets = pydicom.encaps.parse_basic_offsets(f, endianness=endianness)
+
+    if len(basic_offsets) == 0:
+        raise Exception("No basic offsets found")
+
+    start_pos = f.tell() + 8
+    while frame_index < len(basic_offsets)-1:
+        frame_size = basic_offsets[frame_index+1]-basic_offsets[frame_index]
+        end_pos = start_pos + frame_size
+
+        frames.append(
+            {
+                "frame_number": frame_index,
+                "frame_size": frame_size,
+                "start_pos": start_pos,
+                "end_pos": end_pos,
+                "pixel_data_pos": pixel_data_pos,
+            }
+        )
+
+        frame_index += 1
+        start_pos = end_pos
+
+    return frames
+
+def legacy_extract_frames(f, number_of_frames, pixel_data_pos, frame_limit, start_pos, frame_index):
+    frame_delimeter = b"\xfe\xff\x00\xe0"
+    frames = []
+    f.seek(0)
+
+    while frame_index <= number_of_frames and frame_index <= frame_limit:
+        f.seek(start_pos)
+
+        file_content = f.read(100)
+        delimiter = file_content.find(frame_delimeter)
+
+        if delimiter == -1:
+            break
+
+        item_length = struct.unpack("<I", file_content[delimiter + 4 : delimiter + 8])[0]
+
+        start_pos = start_pos + delimiter + 8
+        end_pos = start_pos + item_length
+
+        if start_pos == end_pos:
+            continue
+
+        frames.append(
+            {
+                "frame_number": frame_index,
+                "frame_size": item_length,
+                "start_pos": start_pos,
+                "end_pos": end_pos,
+                "pixel_data_pos": pixel_data_pos,
+            }
+        )
+
+        start_pos = end_pos
+        frame_index += 1
+
+    if frames[0]["frame_size"] < 10000:
+        frames.pop(0) #Remove basic offset table from frames
+
+    return frames
 
 def pixel_frames_from_dcm_metadata_file(
     token: str, db_file: DatabricksFile, frame_limit, last_indexed_frame, last_indexed_start_pos
@@ -88,7 +164,6 @@ def pixel_frames_from_dcm_metadata_file(
         dict: Dictionary containing frame metadata and image dimensions.
     """
     pixel_data_delimiter = b"\xe0\x7f\x10\x00"
-    frame_delimeter = b"\xfe\xff\x00\xe0"
     frames = []
 
     client_kwargs = {
@@ -114,40 +189,19 @@ def pixel_frames_from_dcm_metadata_file(
             start_pos = pixel_data_pos
             frame_index = 0
 
-        item_length = 100
-        end_pos = 0
-
         f.seek(0)
         if is_compressed:
-            while frame_index <= int(ds.get("NumberOfFrames", 1)) and frame_index <= frame_limit:
-                f.seek(start_pos)
-
-                file_content = f.read(item_length + 10)
-                delimiter = file_content.find(frame_delimeter)
-
-                if delimiter == -1:
-                    break
-
-                item_length = struct.unpack("<I", file_content[delimiter + 4 : delimiter + 8])[0]
-
-                start_pos = start_pos + delimiter + 8
-                end_pos = start_pos + item_length
-
-                if start_pos == end_pos:
-                    continue
-
-                frames.append(
-                    {
-                        "frame_number": frame_index,
-                        "frame_size": item_length,
-                        "start_pos": start_pos,
-                        "end_pos": end_pos,
-                        "pixel_data_pos": pixel_data_pos,
-                    }
-                )
-
-                start_pos = end_pos
-                frame_index += 1
+            # try to parse basic offsets first
+            if last_indexed_frame == 0 or last_indexed_start_pos == 0:
+                try:
+                    endianness = '<' if ds.file_meta.TransferSyntaxUID.is_little_endian else '>'
+                    frames = extract_from_basic_offsets(f, pixel_data_pos, endianness)
+                except Exception as e:
+                    logger.warning(f"Error parsing basic offsets: {e}, using fallback method")
+                    frames = legacy_extract_frames(f, number_of_frames, pixel_data_pos, frame_limit, start_pos, frame_index)
+            else:
+                print("Using fallback method with last indexed frame and start pos", last_indexed_frame, last_indexed_start_pos)
+                frames = legacy_extract_frames(f, number_of_frames, pixel_data_pos, frame_limit, start_pos, frame_index)
         else:
             item_length = ds.Rows * ds.Columns * (ds.BitsAllocated // 8)
             for frm_idx in range(number_of_frames):
