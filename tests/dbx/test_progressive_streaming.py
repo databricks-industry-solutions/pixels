29304aab7e8e4d3e3e8dca6c64efc794ec1ef986
Emanuele Rinaldi <emanuele.rinaldi@databricks.com>
2026-03-03 15:47:53 -0500

    import fix

--- tests/dbx/test_progressive_streaming.py
+++ tests/dbx/test_progressive_streaming.py
@@ -472,7 +472,7 @@ class TestProgressiveFileStreamer:
         with tempfile.TemporaryDirectory() as tmpdir:
             streamer = ProgressiveFileStreamer(cache_dir=tmpdir)
 
-            with patch("dbx.pixels.resources.dicom_web.utils.dicom_io._session") as mock_session:
+            with patch("dbx.pixels.resources.dicom_web_gateway.utils.dicom_io._session") as mock_session:
                 mock_session.get.return_value = mock_response
 
                 state1 = streamer.get_or_start_stream(
