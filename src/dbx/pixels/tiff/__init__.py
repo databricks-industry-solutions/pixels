78b1d4f178aa999ccc647baf42771e746beab85b
Douglas Moore <dmoore247@users.noreply.github.com>
2026-07-14 09:19:43 -0400

    remove redundant svs/tiff in favor of wsi

--- src/dbx/pixels/tiff/__init__.py
+++
@@ -1,4 +0,0 @@
-from dbx.pixels.tiff.tiff_meta_extractor import TiffMetaExtractor
-from dbx.pixels.tiff.tiff_vlm_phi_detector import TiffVLMPhiDetector
-
-__all__ = ["TiffMetaExtractor", "TiffVLMPhiDetector"]
